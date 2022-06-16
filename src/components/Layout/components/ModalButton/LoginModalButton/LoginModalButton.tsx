29cb27663deb6647f1c9bfb9a757e2920b3e0b1f
Vadym Babik <vadym.babik@gmail.com>
2022-06-17 01:03:18 +0300

    update LoginModalButton

--- src/components/Layout/components/ModalButton/LoginModalButton/LoginModalButton.tsx
+++ src/components/Layout/components/ModalButton/LoginModalButton/LoginModalButton.tsx
@@ -1,41 +1,27 @@
-import { SubmitModalButton } from '../../../../../Helpers/Buttons/SubmitModalButton';
 import { useLoginForm } from '../../Auth/Hooks/useLoginForm';
-import { InputField } from '../../../../../Helpers/FormFilds/InputField';
-import { InputType } from '../../../../../types';
-import React from 'react';
+
+import { LoginForm } from '../../Auth/Forms/LoginForm';
+
+import { SimpleModalButton } from '../../../../../Helpers/Buttons/SimpleModalButton';
 
 const LoginModalButton = () => {
   const { loginUser, registerFields, validationErrors, resetForm } =
     useLoginForm();
   return (
-    <SubmitModalButton
-      title={'Login'}
-      text={'Sing In'}
+    <SimpleModalButton
+      titleModal={'Login'}
+      buttonText={'Sing In'}
       isLoading={false}
       onSubmit={loginUser}
       onOpen={resetForm}
     >
-      <div className="space-y-6">
-        <InputField
-          label={'Email'}
-          type={InputType.EMAIL}
-          disabled={false}
-          name={registerFields.registerEmail.name}
-          ref={registerFields.registerEmail.ref}
-          onChange={registerFields.registerEmail.onChange}
-          error={validationErrors.emailValidationError}
-        />
-        <InputField
-          label={'Password'}
-          type={InputType.PASSWORD}
-          disabled={false}
-          name={registerFields.registerPassword.name}
-          ref={registerFields.registerPassword.ref}
-          onChange={registerFields.registerPassword.onChange}
-          error={validationErrors.passwordValidationError}
-        />
-      </div>
-    </SubmitModalButton>
+      <LoginForm
+        registerEmail={registerFields.registerEmail}
+        registerPassword={registerFields.registerPassword}
+        emailValidationError={validationErrors.emailValidationError}
+        passwordValidationError={validationErrors.passwordValidationError}
+      />
+    </SimpleModalButton>
   );
 };
 
